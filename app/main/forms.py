dc4093c7efb23f7988e595394731e456d97adf3f
Nikita Volskii <nikitoriy@gmail.com>
2020-12-07 22:53:30 +0300

    WIP: started with some problems with forms and routes. Index page did not render proper values. Meters data input crashed the program. Needs further fixes and refactoring

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from app import db
+from app.models import MetersReader
 from wtforms import SubmitField, FloatField
 from wtforms.validators import DataRequired
 
 
 class InputForm(FlaskForm):
-    electricity = FloatField('Electricity', validators=[DataRequired])
-    cold_water = FloatField('Cold_water', validators=[DataRequired])
-    hot_water = FloatField('Hot_water', validators=[DataRequired])
+    electricity = FloatField('Electricity', validators=[DataRequired()])
+    cold_water = FloatField('Cold_water', validators=[DataRequired()])
+    hot_water = FloatField('Hot_water', validators=[DataRequired()])
     submit = SubmitField('Send')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,17 +1,19 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
+from app import db
 from app.main import main_bp
 from app.main.forms import InputForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, MetersReader
+from datetime import datetime
 
 
 @main_bp.route('/')
 @main_bp.route('/index')
 @login_required
 def index():
-    augu = {'electricity': 16552.0, 'hot_water': 40.755, 'cold_water': 77.835}
-    return render_template('main/index.html', title='Home', augu=augu)
+    meter = MetersReader.query.filter_by(user_id=current_user.id).order_by(MetersReader.date.desc()).first()
+    return render_template('main/index.html', title='Home', meter=meter)
 
 @login_required
 @main_bp.route('/input', methods=['GET', 'POST'])
@@ -22,5 +24,14 @@ def input():
         flash(f'electricity: {form.electricity.data}')
         flash(f'hot_water: {form.hot_water.data}')
         flash(f'cold_water: {form.cold_water.data}')
+        meter = MetersReader(
+            date = datetime.today(),
+            cold_water = form.cold_water.data,
+            hot_water = form.hot_water.data,
+            electricity = form.electricity.data,
+            user_id = current_user.id
+        )
+        db.session.add(meter)
+        db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('main/input.html', title='Enter_values', form=form)
